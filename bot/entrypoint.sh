7fdcd7e21b7b476a56f98d6fbcc5f1ca866767d6
Maximilien Denis <denismaximilien98@gmail.com>
2023-10-08 10:50:19 +0200

    Create container

---
+++ bot/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -f package.json ]; then
+	npm install discord.js;
+fi;
+
+# apk add openrc
+# echo '/usr/bin/node /var/bot/index.js' > /var/bot/script
+# chmod +x script
+# rc-update add /var/bot/script
+# rc-service /var/bot/script start
+
